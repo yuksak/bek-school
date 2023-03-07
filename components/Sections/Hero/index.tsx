928b2d601cba07558923710a6c356dc8b39d492b
yuksak <gulamaier@gmail.com>
2023-03-07 22:04:08 +0500

    refactor: change quote marks

--- components/Sections/Hero/index.tsx
+++ components/Sections/Hero/index.tsx
@@ -4,23 +4,20 @@ import image from '../../../public/images/hero.png'
 export const Hero = () => {
   return (
     <InfoBanner image={image}>
-      <div className="box-content flex w-[582px] flex-col gap-16 p-12 ">
+      <div className="box-content flex w-[582px] flex-col gap-16 p-12">
         <p className="text-grey-darkest">
           В школу дети приходят учиться, и ничто не должно мешать им в этом. Наша цель - воспитать
           всесторонне развитую личность, обладающую собственным мнением, умеющая находить пути
           решения задач любой сложности, личность, которая быстро адаптируется к изменениям вокруг.
           <br />
           <br />
-          &rdquo;Мы хотим видеть ребенка в поисках знаний, а не знания в погоне за ребенком&ldquo; -
-          Бернард Шоу
+          “Мы хотим видеть ребенка в поисках знаний, а не знания в погоне за ребенком” - Бернард Шоу
         </p>
         <div>
           <h1 className="text-2xl font-bold text-grey-darkest">
             Исамухамедова Дильфуза Садриддиновна
           </h1>
-          <p className="font-medium text-grey">
-            Директор НОУ &rdquo;Future education&ldquo;, школа &rdquo;Bek&ldquo;
-          </p>
+          <p className="font-medium text-grey">Директор НОУ "Future education", школа “Bek”</p>
         </div>
       </div>
     </InfoBanner>
