4543af4739e3650b08b57be5cccc8f01fa40bb7c
yuksak <gulamaier@gmail.com>
2023-03-07 15:08:18 +0500

    feat: create breadcrumb checker, add layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { navRoutes } from '../components/Header'
+import Layout from '../components/Layout'
+import { IBreadCrumbs } from '../types'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { pathname } = useRouter()
+  const [breadcrumbs, setBreadCrumbs] = useState<IBreadCrumbs[]>([])
+
+  useEffect(() => {
+    const currentPaths = navRoutes.filter((route) => pathname.includes(route.path))
+    setBreadCrumbs(currentPaths)
+  }, [pathname])
+
+  return (
+    <Layout breadCrumbs={breadcrumbs}>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
