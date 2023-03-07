67b0df5a7d6aafcb6873f83b1ef5c4397b6f5b6e
yuksak <gulamaier@gmail.com>
2023-03-07 15:32:18 +0500

    feat: create differences section

---
+++ components/Sections/Differences/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SectionTitle } from '../../UI'
+
+export const Differences = () => {
+  return (
+    <div>
+      <SectionTitle title="Чем отличается школа “Bek” от других школ" />
+    </div>
+  )
+}
