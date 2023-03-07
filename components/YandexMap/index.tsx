f46df76ec3c0f9a320091218c68118d0ed3f8dc0
yuksak <gulamaier@gmail.com>
2023-03-07 15:11:20 +0500

    feat: create yandex-map component

---
+++ components/YandexMap/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { YMaps, Map, Placemark, ZoomControl } from '@pbe/react-yandex-maps'
+
+export const YandexMap = () => {
+  const defaultState = {
+    center: [41.341176, 69.242969],
+    zoom: 17,
+    controls: [],
+  }
+
+  return (
+    <YMaps>
+      <div className=" overflow-hidden rounded-2xl">
+        <Map defaultState={defaultState} width={1140} height={445}>
+          <ZoomControl options={{ adjustMapMargin: true }} />
+          <Placemark
+            geometry={[41.341176, 69.242969]}
+            options={{
+              preset: 'islands#redCircleDotIcon',
+            }}
+          />
+        </Map>
+      </div>
+    </YMaps>
+  )
+}
