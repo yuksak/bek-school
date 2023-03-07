f11b74507691793671ea22448a9617adf5d79543
yuksak <gulamaier@gmail.com>
2023-03-07 15:33:51 +0500

    feat: create safety section

---
+++ components/Sections/Safety/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SectionTitle } from '../../UI'
+
+export const Safety = () => {
+  return (
+    <div>
+      <SectionTitle title="Безопасность и комфорт" />
+    </div>
+  )
+}
