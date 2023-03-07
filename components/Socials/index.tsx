7a02d5f16e66ab45d52edcec95046210e2c627b3
yuksak <gulamaier@gmail.com>
2023-03-07 15:26:06 +0500

    feat: create socials component

---
+++ components/Socials/index.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+import React from 'react'
+
+import { Instagram, Facebook, Youtube } from 'iconsax-react'
+import { FaTelegramPlane } from 'react-icons/fa'
+
+const socials = [
+  {
+    name: 'Instagram',
+    path: 'https://www.instagram.com/bekschool',
+    logo: <Instagram variant="Bold" className="h-4 w-4" />,
+  },
+  {
+    name: 'Facebook',
+    path: 'https://www.facebook.com/bekschool',
+    logo: <Facebook variant="Bold" className="h-4 w-4" />,
+  },
+  {
+    name: 'Youtube',
+    path: 'https://www.youtube.com/channel/UCtUbbO_EQe03qW7HVZIQ6Ag',
+    logo: <Youtube variant="Bold" className="h-4 w-4" />,
+  },
+  {
+    name: 'Telegram',
+    path: 'https://t.me/bekschool',
+    logo: <FaTelegramPlane className="h-4 w-4" />,
+  },
+]
+
+const Socials = () => {
+  return (
+    <div className="flex gap-4">
+      {socials.map(({ path, name, logo }) => (
+        <Link
+          key={name}
+          href={path}
+          className="rounded-full bg-yellow p-1.5 text-grey-dark hover:text-yellow"
+        >
+          {logo}
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Socials
