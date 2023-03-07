bf182a152137bd87362494224ebc8f2ab4aaef57
yuksak <gulamaier@gmail.com>
2023-03-07 15:32:36 +0500

    feat: create gallery section

---
+++ components/Sections/Gallery/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SectionTitle } from '../../UI'
+
+export const Gallery = () => {
+  return (
+    <div>
+      <SectionTitle title="Галерея" />
+    </div>
+  )
+}
