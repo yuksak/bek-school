a09d957b6ae9554160a2ec793f3a9c60b3b5030e
yuksak <gulamaier@gmail.com>
2023-03-07 15:22:08 +0500

    feat: create contact-card component

---
+++ components/ContactCard/index.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router'
+import { FC, ReactNode } from 'react'
+
+interface IContactedCardProps {
+  title: string
+  content: string
+  path: string
+  children: ReactNode
+}
+
+const ContactCard: FC<IContactedCardProps> = ({ title, content, path, children }) => {
+  const router = useRouter()
+  return (
+    <div
+      onClick={() => router.push(path)}
+      className="box-border flex w-[365px] cursor-pointer flex-row items-center gap-4 rounded-2xl border border-transparent bg-light-dark p-6 hover:border-grey"
+    >
+      <div className=" rounded-xl bg-light p-3">{children}</div>
+      <div className="flex flex-col text-grey-darkest">
+        <span className="text-sm font-medium">{title}</span>
+        <span className="text-lg font-bold">{content}</span>
+      </div>
+    </div>
+  )
+}
+
+export default ContactCard
