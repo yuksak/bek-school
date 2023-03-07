093e6662d8313e6fa94230fa61beb594b45a7e85
yuksak <gulamaier@gmail.com>
2023-03-07 15:31:47 +0500

    feat: create cooperate section

---
+++ components/Sections/Cooperate/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SectionTitle } from '../../UI'
+
+export const Cooperate = () => {
+  return (
+    <div>
+      <SectionTitle title="Школа сотрудничает" />
+    </div>
+  )
+}
