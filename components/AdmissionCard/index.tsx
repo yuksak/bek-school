f72074525e4cfa9f6b55e49d8c10332db4c96a69
yuksak <gulamaier@gmail.com>
2023-03-07 18:25:51 +0500

    refactor: change style

--- components/AdmissionCard/index.tsx
+++ components/AdmissionCard/index.tsx
@@ -9,13 +9,15 @@ interface IAdmissionCardProps {
 
 const AdmissionCard: FC<IAdmissionCardProps> = ({ index, description, info }) => {
   return (
-    <div className="flex w-[752px] flex-row items-center gap-5 rounded-2xl bg-light p-6">
-      <div className="flex h-[51px] w-[51px] items-center justify-center rounded-full bg-yellow text-2xl font-semibold text-grey-darkest">
-        {index}
+    <div>
+      <div className="flex w-[752px] flex-row items-center gap-5 rounded-2xl bg-light p-6">
+        <div className="flex h-[51px] w-[51px] items-center justify-center rounded-full bg-yellow text-2xl font-semibold text-grey-darkest">
+          {index}
+        </div>
+        <p className="text-xl">
+          {description} {info && <Chip>{info}</Chip>}
+        </p>
       </div>
-      <p className="text-xl">
-        {description} {info && <Chip>{info}</Chip>}
-      </p>
     </div>
   )
 }
