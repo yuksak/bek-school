c6f52086e7eb6d4bd8bce64db5e74c6f60bb9900
yuksak <gulamaier@gmail.com>
2023-03-08 21:55:47 +0500

    refactor: import gallery section into about us page

--- pages/about-us/index.tsx
+++ pages/about-us/index.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from 'next'
+import { Gallery } from '../../components/Sections'
 
 const AboutUs: NextPage = () => {
-  return <div>About Us Page</div>
+  return (
+    <div className="mx-auto my-24 flex max-w-[1142px] flex-col gap-24">
+      <Gallery />
+    </div>
+  )
 }
 
 export default AboutUs
