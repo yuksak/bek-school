86017e3fc96b5885b07704054f1f1c975e3f8cc2
yuksak <gulamaier@gmail.com>
2023-03-08 19:55:36 +0500

    feat: add animation to experience section

--- components/Sections/Experience/index.tsx
+++ components/Sections/Experience/index.tsx
@@ -2,27 +2,46 @@ import { useRef, useState } from 'react'
 import SloganBanner from '../../SloganBanner'
 import poster from '../../../public/images/experience-thumbnail.png'
 import Button from '../../Button'
+import { motion } from 'framer-motion'
 
 import { BsPauseFill, BsPlayFill } from 'react-icons/bs'
 import VideoPlayer from '../../VideoPlayer'
 
 export const Experience = () => {
+  const container = {
+    visible: {
+      y: 0,
+      transition: { duration: 1 },
+    },
+    hidden: {
+      y: 200,
+      transition: { duration: 1 },
+    },
+  }
+
   return (
-    <SloganBanner
-      title="18 Лет в лучшей сфере образования"
-      description="Школа «Bek» оказывает образовательные услуги только на русском языке. Основные уроки проводятся по учебной программе, утверждённой МНО, но в процессе преподавания на уроках используются дополнительные материалы с разных источников- как российских, так и зарубежных изданий."
-      link="/experience"
+    <motion.div
+      variants={container}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
     >
-      {/* <VideoPlayer src="https://youtu.be/a43-qyyeZ88" poster="/images/experience-thumbnail.png" /> */}
-      <iframe
-        className="rounded-3xl"
-        width="1142"
-        allowFullScreen
-        height="445"
-        src="https://www.youtube.com/embed/a43-qyyeZ88"
-        title="Школа Бек за Мир!"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      />
-    </SloganBanner>
+      <SloganBanner
+        title="18 Лет в лучшей сфере образования"
+        description="Школа «Bek» оказывает образовательные услуги только на русском языке. Основные уроки проводятся по учебной программе, утверждённой МНО, но в процессе преподавания на уроках используются дополнительные материалы с разных источников- как российских, так и зарубежных изданий."
+        link="/experience"
+      >
+        {/* <VideoPlayer src="https://youtu.be/a43-qyyeZ88" poster="/images/experience-thumbnail.png" /> */}
+        <iframe
+          className="rounded-3xl"
+          width="1142"
+          allowFullScreen
+          height="445"
+          src="https://www.youtube.com/embed/a43-qyyeZ88"
+          title="Школа Бек за Мир!"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        />
+      </SloganBanner>
+    </motion.div>
   )
 }
