c59089eaa2e6eaf3919fae2586a6b545c80111f9
yuksak <gulamaier@gmail.com>
2023-03-08 19:34:22 +0500

    feat: add animation to safety-card

--- components/SafetyCard/index.tsx
+++ components/SafetyCard/index.tsx
@@ -1,13 +1,19 @@
 import { FC } from 'react'
+import { motion } from 'framer-motion'
+import { cardAnimation } from '../../animation'
 
 interface ISafetyCardProps {
   orderNumber: number
   title: string
   description: string
 }
+
 const SafetyCard: FC<ISafetyCardProps> = ({ orderNumber, title, description }) => {
   return (
-    <div className="flex h-[204px] w-[558px] flex-col gap-4 rounded-2xl bg-light-dark p-6">
+    <motion.div
+      variants={cardAnimation}
+      className="flex h-[204px] w-[558px] flex-col gap-4 rounded-2xl bg-light-dark p-6"
+    >
       <div className="flex flex-row items-center gap-4">
         <span className="flex h-9 w-9 items-center justify-center rounded-full bg-yellow font-semibold">
           {orderNumber}
@@ -15,7 +21,7 @@ const SafetyCard: FC<ISafetyCardProps> = ({ orderNumber, title, description }) =
         <h1 className=" text-[22px] font-semibold text-grey-darkest">{title}</h1>
       </div>
       <p className="text-grey">{description}</p>
-    </div>
+    </motion.div>
   )
 }
 
