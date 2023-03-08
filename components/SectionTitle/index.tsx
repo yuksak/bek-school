5ef5f158e338f592c3f5630f1c566410ae97da31
yuksak <gulamaier@gmail.com>
2023-03-08 19:34:47 +0500

    feat: add animation to section-title component

--- components/SectionTitle/index.tsx
+++ components/SectionTitle/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { FC } from 'react'
+import { motion } from 'framer-motion'
 import { MdArrowOutward } from 'react-icons/md'
 
 interface ISectionTitleProps {
@@ -9,8 +10,19 @@ interface ISectionTitleProps {
 }
 
 const SectionTitle: FC<ISectionTitleProps> = ({ title, path, position }) => {
+  const titleVariant = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1 } },
+  }
+
   return (
-    <div className="mb-8">
+    <motion.div
+      className="mb-8"
+      variants={titleVariant}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+    >
       <div
         className={`flex flex-row items-center text-grey-darkest ${
           title && path
@@ -28,7 +40,7 @@ const SectionTitle: FC<ISectionTitleProps> = ({ title, path, position }) => {
           </Link>
         )}
       </div>
-    </div>
+    </motion.div>
   )
 }
 
