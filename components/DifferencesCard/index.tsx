ebb3af71b020b565b72e615efdf6b0c0c13c48d5
yuksak <gulamaier@gmail.com>
2023-03-08 19:33:06 +0500

    feat: add animation to differences card

--- components/DifferencesCard/index.tsx
+++ components/DifferencesCard/index.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react'
-
+import { motion } from 'framer-motion'
+import { cardAnimation } from '../../animation'
 interface IDifferencesCardProps {
   orderNumber: number
   title: string
   description: string
 }
+
 const DifferencesCard: FC<IDifferencesCardProps> = ({ orderNumber, title, description }) => {
   return (
-    <div className="flex h-[245px] w-[365px] flex-col gap-4 rounded-2xl bg-light-dark p-6">
+    <motion.div
+      className="flex h-[245px] w-[365px] flex-col gap-4 rounded-2xl bg-light-dark p-6"
+      variants={cardAnimation}
+    >
       <div className="flex flex-row items-center gap-4">
         <div className="flex h-12 w-12 items-center justify-center rounded-full bg-yellow font-semibold">
           {orderNumber}
@@ -15,7 +20,7 @@ const DifferencesCard: FC<IDifferencesCardProps> = ({ orderNumber, title, descri
         <h1 className="w-[249px] text-lg font-semibold text-grey-darkest">{title}</h1>
       </div>
       <p className="text-grey">{description}</p>
-    </div>
+    </motion.div>
   )
 }
 
