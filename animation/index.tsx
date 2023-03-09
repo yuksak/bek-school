2b3aac2af4a8b25c23fbaa150480326eee85e18a
yuksak <gulamaier@gmail.com>
2023-03-09 16:24:57 +0500

    refactor: add new animations

--- animation/index.tsx
+++ animation/index.tsx
@@ -5,3 +5,34 @@ export const cardAnimation = {
     opacity: 1,
   },
 }
+
+export const cardContainerAnimation = {
+  staggerChildren: 0.2,
+  delayChildren: 0.1,
+}
+
+export const sectionAnimation = {
+  visible: {
+    y: 0,
+    transition: { duration: 0.5 },
+  },
+  hidden: {
+    y: 200,
+    transition: { duration: 0.5 },
+  },
+}
+
+export const titleVariant = {
+  hidden: { opacity: 0, y: -50 },
+  visible: { opacity: 1, y: 0, transition: { delay: 0.2, duration: 0.5 } },
+}
+
+export const textVariant = {
+  hidden: { opacity: 0, y: -50 },
+  visible: { opacity: 1, y: 0, transition: { delay: 0.3, duration: 0.5 } },
+}
+
+export const logoVariant = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { delay: 0.4, duration: 0.5 } },
+}
