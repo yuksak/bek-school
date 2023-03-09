8df9978536b2d1fa5770806115e34ae4eb609241
yuksak <gulamaier@gmail.com>
2023-03-09 21:14:56 +0500

    refactor: change style

--- components/DifferencesCard/index.tsx
+++ components/DifferencesCard/index.tsx
@@ -14,10 +14,10 @@ const DifferencesCard: FC<IDifferencesCardProps> = ({ orderNumber, title, descri
       variants={cardAnimation}
     >
       <div className="flex flex-row items-center gap-4">
-        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-yellow font-semibold">
+        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-yellow text-2xl font-semibold">
           {orderNumber}
         </div>
-        <h1 className="w-[249px] text-lg font-semibold text-grey-darkest">{title}</h1>
+        <h1 className="w-[249px] text-lg font-semibold leading-6 text-grey-darkest">{title}</h1>
       </div>
       <p className="text-grey">{description}</p>
     </motion.div>
