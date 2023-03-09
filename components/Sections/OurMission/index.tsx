3fa9385837cceb421c4c859ec708abb6e07cceaf
yuksak <gulamaier@gmail.com>
2023-03-09 21:16:11 +0500

    refactor: change style

--- components/Sections/OurMission/index.tsx
+++ components/Sections/OurMission/index.tsx
@@ -1,10 +1,17 @@
+import { FC } from 'react'
 import { motion } from 'framer-motion'
 
 import SloganBanner from '../../SloganBanner'
-import missionImage from '../../../public/images/sec-mission-image.png'
+import image1 from '../../../public/images/sec-mission-image-1.png'
+import image2 from '../../../public/images/sec-mission-image-2.png'
 import { sectionAnimation } from '../../../animation'
 
-export const OurMission = () => {
+interface IOurMissionProps {
+  text: 'first' | 'second'
+  image: 'teacher' | 'pupil'
+}
+
+export const OurMission: FC<IOurMissionProps> = ({ text, image }) => {
   return (
     <motion.div
       variants={sectionAnimation}
@@ -14,11 +21,12 @@ export const OurMission = () => {
     >
       <SloganBanner
         title="Наша миссия - помогать всем и каждому"
-        description="Школа «Bek» оказывает образовательные услуги только на русском языке. Основные уроки
-      проводятся по учебной программе, утверждённой МНО, но в процессе преподавания на уроках
-      используются дополнительные материалы с разных источников - как российских, так и
-      зарубежных изданий."
-        image={missionImage}
+        description={
+          text === 'first'
+            ? 'Школа «Bek» оказывает образовательные услуги только на русском языке. Основные уроки проводятся по учебной программе, утверждённой МНО, но в процессе преподавания на уроках используются дополнительные материалы с разных источников - как российских, так и зарубежных изданий.'
+            : 'Школа ВЕК и детские сады BEK-KICHKINTOY принимают детей с 2 до 17 лет, 2-7 - детский сад, а с 7-17 - школа. \n \nВесь процесс обучения направлен на то, чтобы ребенок был в постоянном развитии, учебный процесс давался легко и приносил радость от достигнутых результатов. По нашему мнению каждый ребенок имеет право получать качественное образование, не зависимо от его уровня.'
+        }
+        image={image === 'pupil' ? image1 : image2}
       />
     </motion.div>
   )
