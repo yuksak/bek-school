d329b5c81a2f7154b8bc420915d1be3c7fbe0590
yuksak <gulamaier@gmail.com>
2023-03-09 21:15:45 +0500

    refactor: change style

--- components/InfoBanner/index.tsx
+++ components/InfoBanner/index.tsx
@@ -9,8 +9,8 @@ const InfoBanner: FC<IInfoBannerProps> = ({ children, image }) => {
   return (
     <div className="flex h-[410px] flex-row items-center justify-between rounded-3xl bg-yellow-light">
       {children}
-      <div className="relative flex h-[410px] w-[419px] justify-center rounded-3xl">
-        <Image src={image} alt="info-banner" className="absolute" />
+      <div className="relative flex h-[410px] w-[419px] rounded-3xl">
+        <Image priority={true} src={image} alt="info-banner" className=" absolute rounded-3xl" />
       </div>
     </div>
   )
