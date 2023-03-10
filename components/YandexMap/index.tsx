79ee19c8fe00f7301b966002167d38c48c3f6bef
yuksak <gulamaier@gmail.com>
2023-03-10 19:39:34 +0500

    refactor: change radius of yandex map

--- components/YandexMap/index.tsx
+++ components/YandexMap/index.tsx
@@ -17,7 +17,10 @@ const YandexMap = () => {
         initial="hidden"
         whileInView="visible"
       >
-        <Map defaultState={defaultState} className="h-[296px] w-full max-w-[1140px] md:h-[445px]">
+        <Map
+          defaultState={defaultState}
+          className="h-[296px] w-full max-w-[1140px] rounded-2xl md:h-[445px]"
+        >
           <ZoomControl options={{ adjustMapMargin: true }} />
           <Placemark
             geometry={[41.341176, 69.242969]}
